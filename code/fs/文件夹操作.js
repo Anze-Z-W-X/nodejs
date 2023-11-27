d02d375e314578c29013fd98dba74021f1d24269
Anze <2290434225@qq.com>
2023-11-27 14:50:08 +0800

    day2

--- code/fs/文件夹操作.js
+++ code/fs/文件夹操作.js
@@ -8,10 +8,38 @@ const fs = require('fs');
 //     console.log('操作成功');
 // });
 
-fs.mkdir('./a/b/c',{recursive:true},err =>{
-    if(err){
-        console.log('操作失败');
-        return;
-    }
-    console.log('操作成功');
-})
+// fs.mkdir('./a/b/c',{recursive:true},err =>{
+//     if(err){
+//         console.log('操作失败');
+//         return;
+//     }
+//     console.log('操作成功');
+// })
+
+//读取文件夹
+// fs.readdir('./',(err,data) =>{
+//     if(err){
+//         console.log('操作失败');
+//         return;
+//     }
+//     console.log(data);
+// })
+
+//删除文件夹
+//不推荐使用
+// fs.rmdir('./a',{recursive:true},err =>{
+//     if(err){
+//         console.log('操作失败');
+//         return;
+//     }
+//     console.log('操作成功');
+// });
+
+//建议使用
+// fs.rm('./a',err =>{
+//     if(err){
+//         console.log('操作失败');
+//         return;
+//     }
+//     console.log('操作成功');
+// });
