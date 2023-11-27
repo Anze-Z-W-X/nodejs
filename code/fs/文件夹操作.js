0fa8b9c189dcef5a0a6240ed2c8352900c401b7e
Anze <2290434225@qq.com>
2023-11-27 14:03:22 +0800

    day2

--- code/fs/文件夹操作.js
+++ code/fs/文件夹操作.js
@@ -1,9 +1,17 @@
 const fs = require('fs');
 
-fs.mkdir('./html',err =>{
+// fs.mkdir('./html',err =>{
+//     if(err){
+//         console.log('操作失败');
+//         return;
+//     }
+//     console.log('操作成功');
+// });
+
+fs.mkdir('./a/b/c',{recursive:true},err =>{
     if(err){
         console.log('操作失败');
         return;
     }
     console.log('操作成功');
-});
+})
